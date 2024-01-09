5be5516f022f5960d80ff58b7cd2cf30836e0a4e
PEYRAT Aurélien <aure1814fr@gmail.com>
2024-01-09 16:27:44 +0100

    update javascript

--- assets/script.js
+++ assets/script.js
@@ -1,67 +1,96 @@
-const arrow_left = document.getElementsByClassName("arrow_left")[0];
-const arrow_right = document.getElementsByClassName("arrow_right")[0];
-const slides = document.getElementsByClassName("banner-img")[0];
-const paragraph = document.getElementsByClassName("paragraph")[0];
-const dot_1 = document.getElementsByClassName("dot-1")[0];
-const dot_2 = document.getElementsByClassName("dot-2")[0];
-const dot_3 = document.getElementsByClassName("dot-3")[0];
-const dot_4 = document.getElementsByClassName("dot-4")[0];
-let image = 1;
+"use strict";
 
-arrow_left.addEventListener('click', () => {
-	console.log('La fleche gauche a été cliqué !');
-	image = image - 1;
-	updateImage();
-	console.log(image);
-});
+// ********** CONSTANTS **********
 
-arrow_right.addEventListener('click', () => {
-    console.log('La flèche droite a été cliquée !');
-    image = image + 1;
-	updateImage();
-	console.log(image);
-});
+const slides = [
+  {
+    "image": "slide1.jpg",
+    "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+  },
+  {
+    "image": "slide2.jpg",
+    "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+  },
+  {
+    "image": "slide3.jpg",
+    "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+  },
+  {
+    "image": "slide4.png",
+    "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+  }
+];
 
-function updateImage() {
-	if (image === 1) {
-			slides.src = './assets/images/slideshow/slide1.jpg';
-			paragraph.innerHTML = 'Impressions tous formats<span>en boutique et en ligne</span>';
-			dot_1.style.backgroundColor = "white";
-			dot_2.style.backgroundColor = "transparent";
-			dot_4.style.backgroundColor = "transparent";
-		}
+const arrowLeft   = document.querySelector(".arrow-left");
+const arrowRight  = document.querySelector(".arrow-right");
+const slide       = document.querySelector(".banner-img");
+const paragraph   = document.querySelector(".paragraph");
+const dots        = document.querySelector(".dots");
 
-	if (image === 2) {
-			slides.src = "./assets/images/slideshow/slide2.jpg";
-			paragraph.innerHTML = 'Tirages haute definition grand format<span>pour vos bureaux et events</span>';
-			dot_2.style.backgroundColor = "white";
-			dot_3.style.backgroundColor = "transparent";
-			dot_1.style.backgroundColor = "transparent";
-		}
-	
-	if (image === 3) {
-			slides.src = "./assets/images/slideshow/slide3.jpg";
-			paragraph.innerHTML = "Grand choix de couleurs <span>de CMJN aux pantones</span>"
-			dot_3.style.backgroundColor = "white";
-			dot_2.style.backgroundColor = "transparent";
-			dot_4.style.backgroundColor = "transparent";
-		}
-	
-	if (image === 4) {
-			slides.src = "./assets/images/slideshow/slide4.png";
-			paragraph.innerHTML = "Autocollants <span>avec découpe laser sur mesure</span>"
-			dot_4.style.backgroundColor = "white";
-			dot_1.style.backgroundColor = "transparent";
-			dot_3.style.backgroundColor = "transparent";
-		}
+// ********** VARIABLES **********
 
-	if (image === 5) {
-		image = 1;
-		updateImage();
-	}
+let index = 0;
 
-	if (image === 0) {
-		image = 4;
-		updateImage();
-	}
+// ********** FUNCTIONS **********
+
+/**
+ * ? CHANGE SLIDE
+ * * Updates the slide and content based on the given index.
+ */
+function changeSlide(index) {
+  const allDots       = dots.children;
+  slide.src           = "./assets/images/slideshow/" + slides[index].image;
+  paragraph.innerHTML = slides[index].tagLine;
+
+  for (let i = 0; i < allDots.length; i++) {
+    allDots[i].classList.toggle("dot-selected", i === index);
+  }
 }
+
+/**
+ * ? SLIDE LEFT
+ * * Slides the content to the left.
+ */
+function slideLeft() {
+  index = (index - 1 + slides.length) % slides.length;
+  changeSlide(index);
+}
+
+/**
+ * ? SLIDE RIGHT
+ * * Slides the current slide to the right.
+ */
+function slideRight() {
+  index = (index + 1) % slides.length;
+  changeSlide(index);
+}
+
+/**
+ * ? ADD LISTENERS
+ * * Adds event listeners to the arrowLeft and arrowRight elements.
+ */
+function addListeners() {
+  arrowLeft.addEventListener('click', slideLeft);
+  arrowRight.addEventListener('click', slideRight);
+}
+
+/**
+ * ? CREATE DOTS
+ * * Creates dots for each slide and adds event listeners for click events.
+ */
+function createDots() {
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("span");
+
+    dot.classList.add("dot");
+    dot.addEventListener("click", () => changeSlide(i));
+
+    if (i === index) dot.classList.add("dot-selected");
+    dots.appendChild(dot);
+  }
+}
+
+// ********** MAIN CODE **********
+
+createDots();
+addListeners();
